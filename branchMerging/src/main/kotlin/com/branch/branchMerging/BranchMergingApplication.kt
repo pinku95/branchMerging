83783efe7dbf67f0cdd41adc283ca2dee0d83eb9
pinku95 <gouda.pinku664@gmail.com>
2025-02-20 07:52:31 +0530

    Initial commit in master

---
+++ branchMerging/src/main/kotlin/com/branch/branchMerging/BranchMergingApplication.kt
@@ -0,0 +1,11 @@
+package com.branch.branchMerging
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BranchMergingApplication
+
+fun main(args: Array<String>) {
+	runApplication<BranchMergingApplication>(*args)
+}
---
+++ branchMerging/src/test/kotlin/com/branch/branchMerging/BranchMergingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.branch.branchMerging
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BranchMergingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
